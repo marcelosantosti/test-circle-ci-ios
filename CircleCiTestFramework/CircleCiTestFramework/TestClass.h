5a9ff133eb377547e04e03940382a7f3e22cac30
Marcelo Santos <marcelo@connectedinteractive.com>
2018-01-15 15:59:44 -0200

    - Adding framework to test.

---
+++ CircleCiTestFramework/CircleCiTestFramework/TestClass.h
@@ -0,0 +1,15 @@
+//
+//  TestClass.h
+//  CircleCiTestFramework
+//
+//  Created by Marcelo Santos on 15/01/18.
+//  Copyright © 2018 Marcelo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TestClass : NSObject
+
+- (NSString*) testMethod;
+    
+@end
---
+++ CircleCiTestFramework/CircleCiTestFramework/TestClass.m
@@ -0,0 +1,18 @@
+//
+//  TestClass.m
+//  CircleCiTestFramework
+//
+//  Created by Marcelo Santos on 15/01/18.
+//  Copyright © 2018 Marcelo. All rights reserved.
+//
+
+#import "TestClass.h"
+
+@implementation TestClass
+
+- (NSString*) testMethod
+{
+    return @"test";
+}
+    
+@end
