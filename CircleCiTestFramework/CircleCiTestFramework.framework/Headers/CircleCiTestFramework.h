fca4680155d75726afd6733298f2a6d62909189b
Marcelo Santos <marcelo@connectedinteractive.com>
2018-01-18 14:40:11 -0200

    - Adding lane to generate framework.

---
+++ CircleCiTestFramework/CircleCiTestFramework.framework/Headers/CircleCiTestFramework.h
@@ -0,0 +1,19 @@
+//
+//  CircleCiTestFramework.h
+//  CircleCiTestFramework
+//
+//  Created by Marcelo Santos on 15/01/18.
+//  Copyright © 2018 Marcelo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CircleCiTestFramework.
+FOUNDATION_EXPORT double CircleCiTestFrameworkVersionNumber;
+
+//! Project version string for CircleCiTestFramework.
+FOUNDATION_EXPORT const unsigned char CircleCiTestFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CircleCiTestFramework/PublicHeader.h>
+
+
