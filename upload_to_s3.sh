b9158d5df683edec7eb780fcd29281ed1e4fb7b2
Marcelo Santos <marcelo@connectedinteractive.com>
2018-01-18 16:38:39 -0200

    fixing aws path

--- upload_to_s3.sh
+++ upload_to_s3.sh
@@ -6,7 +6,7 @@ ZIP_FILE_NAME="library.zip"
 CONTENT_TYPE="application/zip"
 BUCKET_NAME="connected-builds"
 
-aws s3 cp "$ZIP_FILE_NAME/$ZIP_FILE_PATH" \
+aws s3 cp "$ZIP_FILE_PATH/$ZIP_FILE_NAME" \
 		s3://$BUCKET_NAME/$ZIP_FILE_NAME \
 		--acl "public-read" \
 		--content-type "$CONTENT_TYPE"
